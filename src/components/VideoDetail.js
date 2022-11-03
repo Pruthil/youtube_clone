5284037c6f897bb422aea0d405fee1dcf332caa2
Pruthil <pruthilgandhi@gmail.com>
2022-11-04 01:56:23 +0530

    func comp

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,29 +1,22 @@
 import React from "react";
 
-class VideoDetail extends React.Component {
-  render() {
-    if (!this.props.video) {
-      return <div>Loading .... </div>;
-    } else {
-      const videoSrc = `https://www.youtube.com/embed/${this.props.video.id.videoId}`;
-      return (
-        <>
-          <div className="ui embed">
-            <iframe
-              title={this.props.video.snippet.title}
-              src={videoSrc}
-            ></iframe>
-          </div>
-          <div className="ui segment">
-            <h4 className="ui header">{this.props.video.snippet.title}</h4>
-            <div className="ui content">
-              {this.props.video.snippet.description}
-            </div>
-          </div>
-        </>
-      );
-    }
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading .... </div>;
+  } else {
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+    return (
+      <>
+        <div className="ui embed">
+          <iframe title={video.snippet.title} src={videoSrc}></iframe>
+        </div>
+        <div className="ui segment">
+          <h4 className="ui header">{video.snippet.title}</h4>
+          <div className="ui content">{video.snippet.description}</div>
+        </div>
+      </>
+    );
   }
-}
+};
 
 export default VideoDetail;
